26fcdb5cd76c0c64d29ea8619267317b0b5b1950
JD <josh-dingri@hotmail.co.uk>
2015-01-27 01:15:26 +0000

    Account and logins randomly generated

---
+++ Coursework/Implementation/Files/AddingAccounts.py
@@ -0,0 +1,128 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+import sqlite3
+import random
+import string
+
+class AddUserAccounts(QMainWindow):
+    """Program will allow IT Staff to add user accounts"""
+
+    def __init__(self):
+        super().__init__()
+        self.ProgramLayout()
+
+    def ProgramLayout(self):
+        self.grid = QGridLayout()
+        self.horizontal = QHBoxLayout()
+        self.horizontal2 = QHBoxLayout()
+        self.horizontal3 = QHBoxLayout()
+        self.horizontal4 = QHBoxLayout()
+        self.vertical = QVBoxLayout()
+
+        self.FName_Lbl = QLabel("First Name")
+        self.FName_LE = QLineEdit()
+        
+        self.LName_Lbl = QLabel("Last Name")
+        self.LName_LE = QLineEdit()
+
+        self.AccessLevel_Lbl = QLabel("Access Level")
+        self.AccessLevel_CB = QComboBox()
+        self.AccessLevel_CB.addItem("Admin")
+        self.AccessLevel_CB.addItem("Manager")
+        self.AccessLevel_CB.addItem("Staff")
+
+        self.grid.addWidget(self.FName_Lbl,0,0)
+        self.grid.addWidget(self.FName_LE,0,1)
+
+        self.grid.addWidget(self.LName_Lbl,1,0)
+        self.grid.addWidget(self.LName_LE,1,1)
+
+        self.grid.addWidget(self.AccessLevel_Lbl,2,0)
+        self.grid.addWidget(self.AccessLevel_CB,2,1)
+
+        self.UsernameResult  = QLineEdit()
+        self.RandomPassword = QLineEdit()
+        self.AccessLevel = QLineEdit()
+
+        self.GenerateAccount_btn = QPushButton("Create Account")
+        self.GenerateAccount_btn.setFixedWidth(300)
+
+        self.AddAccount = QPushButton("Add Account")
+
+        self.horizontal.addWidget(self.GenerateAccount_btn)
+
+        self.Usernamelbl = QLabel("Username")
+        self.Usernamelbl.setAlignment(Qt.AlignCenter)
+        self.Passwordlbl = QLabel("Password")
+        self.Passwordlbl.setAlignment(Qt.AlignCenter)
+        self.AccessLevl = QLabel("Access Level")
+        self.AccessLevl.setAlignment(Qt.AlignCenter)
+
+        self.horizontal4.addWidget(self.Usernamelbl)
+        self.horizontal4.addWidget(self.Passwordlbl)
+        self.horizontal4.addWidget(self.AccessLevl)
+
+        self.horizontal2.addWidget(self.UsernameResult)
+        self.horizontal2.addWidget(self.RandomPassword)
+        self.horizontal2.addWidget(self.AccessLevel)
+
+        self.horizontal3.addWidget(self.AddAccount)
+
+        self.vertical.addLayout(self.grid)
+        self.vertical.addLayout(self.horizontal)
+        self.vertical.addStretch(1)
+        self.vertical.addLayout(self.horizontal4)
+        self.vertical.addLayout(self.horizontal2)
+        self.vertical.addLayout(self.horizontal3)
+
+        self.horizontal4.setSpacing(0)
+
+
+        self.window = QWidget()
+        self.window.setLayout(self.vertical)
+        self.setCentralWidget(self.window)
+
+        self.GenerateAccount_btn.clicked.connect(self.GenerateAccount)
+
+    def GenerateAccount(self):
+        self.FirstName = self.FName_LE.text()
+        self.LastName = self.LName_LE.text()
+        self.GetAccessLevel = self.AccessLevel_CB.currentText()
+
+        username = (self.FirstName[0]+self.LastName+str(random.randint(1,10)))
+
+
+        Password_chars = string.ascii_uppercase + string.digits + string.ascii_lowercase
+        Password_size = 7
+
+        Password = ("".join((random.choice(Password_chars)) for count in range(Password_size)))
+
+        self.UsernameResult.setText(username)
+        self.RandomPassword.setText(Password)
+        self.AccessLevel.setText(self.GetAccessLevel)
+
+        self.AddAccount.clicked.connect(self.AddAccountDBConnection)
+
+
+    def AddAccountDBConnection(self):
+        values = None
+        values = (self.UsernameResult.text(),self.RandomPassword.text(),self.AccessLevel.text())
+        with sqlite3.connect("Accounts.db") as db:
+            cursor = db.cursor()
+            sql = "insert into Accounts(Username,Password,Access_Level) values (?,?,?)"
+            cursor.execute(sql,values)
+            db.commit()            
+        
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    launcher = AddUserAccounts()
+    launcher.resize(500,300)
+    launcher.show()
+    launcher.raise_()
+    app.exec_()
+        
+        
+
