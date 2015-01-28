8ad27e03db5db2664ecdc90be5ac546101a4355c
30597 <30597@PC004229.coll.lr.local>
2015-01-28 15:19:46 +0000

    Searching half done

--- Coursework/Implementation/Files/SearchStaffAdmin.py
+++ Coursework/Implementation/Files/SearchStaffAdmin.py
@@ -1,6 +1,8 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
+import sqlite3
+
 
 class SearchStaff(QMainWindow):
     """Admin search staff window"""
@@ -13,17 +15,29 @@ class SearchStaff(QMainWindow):
                 
         DatabaseLbl = QLabel("Department")
         DatabaseLbl.setFont(QFont("Calibri",20))
-        Database_CB = QComboBox()
-        Database_CB.setFixedHeight(30)
-        Database_CB.setFixedWidth(150)
-        Items = ['Financing','Marketing']
-        Database_CB.addItems(Items)
+        self.Database_CB = QComboBox()
+        self.Database_CB.setFixedHeight(30)
+        self.Database_CB.setFixedWidth(150)
+        
+        with sqlite3.connect("Volac.db") as db:
+            self.cursor = db.cursor()
+            sql = "SELECT DepartmentName FROM Department"
+            self.cursor.execute(sql)
+            db.commit()
+            
+        field_names = []
+        
+        for self.row, form in enumerate(self.cursor): 
+                for self.column, item in enumerate(form): 
+                    field_names.append(item)
+
+        self.Database_CB.addItems(field_names)
         
 
         SearchLbl = QLabel("Name")
-        Search_LE = QLineEdit()
-        Search_LE.setFixedWidth(150)
-        Search_LE.setFixedHeight(25)
+        self.Search_LE = QLineEdit()
+        self.Search_LE.setFixedWidth(150)
+        self.Search_LE.setFixedHeight(25)
         SearchLbl.setFont(QFont("Calibri",20))
 
         self.Back_btn = QPushButton("Back")
@@ -35,11 +49,11 @@ class SearchStaff(QMainWindow):
         self.grid.addWidget(self.Back_btn,0,0)
         self.grid.addWidget(space,1,0)
         self.grid.addWidget(DatabaseLbl,1,1)
-        self.grid.addWidget(Database_CB,1,2)
+        self.grid.addWidget(self.Database_CB,1,2)
         self.grid.addWidget(space,1,3)
 
         self.grid.addWidget(SearchLbl,2,1)
-        self.grid.addWidget(Search_LE,2,2)
+        self.grid.addWidget(self.Search_LE,2,2)
 
         self.grid.setVerticalSpacing(20)
 
@@ -48,6 +62,17 @@ class SearchStaff(QMainWindow):
         window_widget = QWidget()
         window_widget.setLayout(self.verticle)
         self.setCentralWidget(window_widget)
+
+        self.Search_LE.textChanged.connect(self.ShowResults)
+
+    def ShowResults(self):
+        print(self.Search_LE.text())
+        self.search_results_table = QTableView()
+        with sqlite3.connect("Volac.db") as db:
+            self.cursor = db.cursor()
+            sql = "SELECT DepartmentID FROM Staff
+        
+        
 
 
 if __name__ == "__main__":
